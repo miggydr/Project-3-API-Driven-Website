c333233cc3bd244e17308c2934fa5cf66be80ac4
miggydr <miggyderamos@gmail.com>
2024-06-17 12:03:19 +0800

    fixed search box

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const generateGif = (event) => {
     loader.style.display = "block";
     document.querySelector(".wrapper").style.display = "none";
 
-    let q = document.getElementById("search-box").value;
+    let search = document.getElementById("search-box").value;
     //Number of gifs to be displayed
     let gifCount = 15;
 
-    let finalURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}&limit=${gifCount}&offset=0&rating=g&lang=en`;
+    let finalURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=${gifCount}&offset=0&rating=g&lang=en`;
     document.querySelector(".wrapper").innerHTML = "";
 
     //make call to API
